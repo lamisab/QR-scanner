64f263b705454c17c5bd2b2499b628ee3cf52ada
norah <norahaltamimi@norahs>
2022-11-29 17:00:34 +0300

    new branch

--- lib/bindings.dart
+++ lib/bindings.dart
@@ -1,13 +1,10 @@
 import 'package:get/get.dart';
-
 import 'controller/admin_contrller.dart';
 import 'controller/user_controller.dart';
-
 class Binding implements Bindings {
   @override
   void dependencies() {
     Get.put<AdminController>(AdminController(), tag: "data", permanent: true);
     Get.put<UserController>(UserController(),tag: "data", permanent: true);
-
   }
 }
