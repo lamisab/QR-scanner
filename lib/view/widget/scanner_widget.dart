1fbc252ca255317ba5cfd4606e9f0c8ec7c68a72
norah <norahaltamimi@norahs>
2022-11-30 11:58:35 +0300

    admin page with camera

--- lib/view/widget/scanner_widget.dart
+++ lib/view/widget/scanner_widget.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ScannerWidget extends StatefulWidget {
+import 'camera_widget.dart';
+
+class ScannerWidget extends StatelessWidget {
   const ScannerWidget({super.key});
 
-  @override
-  State<ScannerWidget> createState() => _ScannerWidgetState();
-}
-
-class _ScannerWidgetState extends State<ScannerWidget> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(height: MediaQuery.of(context).size.height *0.2640,),
-        Container(
-          height: MediaQuery.of(context).size.height *0.3544 ,
-          width: MediaQuery.of(context).size.width *0.95,
-          color: Colors.blue,
-        ),
-         SizedBox(height: MediaQuery.of(context).size.height *0.1420,),
+        Container(height: MediaQuery.of(context).size.height *0.100,),
+        CameraWidget(),
+         SizedBox(height: MediaQuery.of(context).size.height *0.0420,),
       ],
     );
   }
